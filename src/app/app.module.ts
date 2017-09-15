bb3b7ac673e260ede1b79cbb0bde39f2441e74c6
PWHsueh <pwhsueh@gmail.com>
2017-09-15 14:44:15 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
 
+const appRoutes: Routes = [
+{ path: 'crisis-center', component: CrisisListComponent },
+// { path: 'hero/:id',      component: HeroDetailComponent },
+{
+path: 'heroes',
+component: HeroListComponent,
+data: { title: 'Heroes List' }
+},
+// { path: '',
+// redirectTo: '/heroes',
+// pathMatch: 'full'
+// },
+{ path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroListComponent,
+    PageNotFoundComponent,
+    CrisisListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
